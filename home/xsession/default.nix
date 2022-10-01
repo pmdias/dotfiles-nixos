f7f776e2fe24fa9463ef6e52fcda6412ce98418b
Pedro Dias <pedrodias.miguel@gmail.com>
2022-10-01 21:14:18 +0200

    Change i3 configuration to add gaps and use the theme colors

--- home/xsession/default.nix
+++ home/xsession/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
-
-{
+let
+  colorScheme = import ../themes/tokyonight/colorscheme.nix;
+in {
   xsession = {
     enable = true;
 
@@ -11,6 +12,17 @@
         terminal = "kitty";
         modifier = "Mod4";
         window.titlebar = false;
+	gaps = { inner = 10; outer = 0; };
+	bars = [];
+	colors = {
+          focused = {
+	    background = "${colorScheme.terminal.background}";
+	    border = "${colorScheme.base_colors.red}";
+	    childBorder = "${colorScheme.base_colors.red}";
+	    indicator = "${colorScheme.base_colors.red}";
+	    text = "${colorScheme.terminal.foreground}";
+	  };
+	};
       };
       extraConfig = ''
         exec --no-startup-id feh --bg-scale ${config.xdg.dataHome}/wallpaper.png
