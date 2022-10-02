dfe1796113d62956dd003f8a4c3c71afee39e4f1
Pedro Dias <pedrodias.miguel@gmail.com>
2022-10-02 19:22:56 +0200

    Change git configuration so that default branch is called main

--- home/git/default.nix
+++ home/git/default.nix
@@ -4,5 +4,11 @@
     enable = true;
     userEmail = "pedrodias.miguel@gmail.com";
     userName = "Pedro Dias";
+
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+    };
   };
 }
